26a892ac78f52e2a25b2b3b4542eb9c3c7314c7d
harshabapatla <hvardhan0098@gmail.com>
2023-07-12 10:44:46 -0700

    13

--- app.js
+++ app.js
@@ -71,13 +71,14 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request =require('request');
 const https = require("https");
 
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
