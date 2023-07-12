e234fc7a2d05ac940032b75f84b43b0c9ba44ad8
harshabapatla <hvardhan0098@gmail.com>
2023-07-11 23:52:27 -0700

    testing again

--- app.js
+++ app.js
@@ -127,6 +127,6 @@ app.post("/failure", function(req,res){
 });
 
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server is running on port ");
+app.listen(process.env.PORT||3000, function () {
+  console.log("Server is running");
 });
